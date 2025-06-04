d12cc01f7f46b0f0f8cbcfa810c94dc5d10976a9
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2025-06-04 09:13:05 +0200

    ⬆️ `pnpm upgrade` and `pnpm update`

--- test/user/REQ-1.test.ts
+++ test/user/REQ-1.test.ts
@@ -1,9 +1,9 @@
 import {until} from "@adam-rocska/until";
 
 test(`'until' should resolve early, if the predicate is true from the very beginning.`, async () => {
-  jest.spyOn(global.Date, 'now').mockImplementation(() => 0);
-  jest.spyOn(global, 'setInterval');
-  jest.spyOn(global, 'clearInterval');
+  jest.spyOn(global.Date, `now`).mockImplementation(() => 0);
+  jest.spyOn(global, `setInterval`);
+  jest.spyOn(global, `clearInterval`);
 
   await until(() => true);
 
